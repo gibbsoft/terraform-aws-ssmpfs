fc65c8eac7e2f857c6d515d03a32986b18e5c11f
Nigel Gibbs <nigel@gibbsoft.com>
2022-10-23 06:39:34 +0100

    Fix small typo in comment

--- demo/locals.tf
+++ demo/locals.tf
@@ -19,6 +19,6 @@
 locals {
   text = "Hello World"
 
-  # This generates a large arbitrary string 16k in size, image it's a cert chain or something.
+  # This generates a large arbitrary string 16k in size, imagine it's a cert chain or something.
   some_string = join("", [for v in split("", sha512(local.text)) : sha512(local.text)])
 }
